c8dbbe57da37c3f575c9fc90a6aca31f4588acb3
Nick Aversano <nick23hi@gmail.com>
2024-11-30 22:11:54 -0600

    fix the bugs

--- make_board.py
+++ make_board.py
@@ -57,19 +57,19 @@ def score_board(board):
     # cols
     for x in range(5):
         for y in range(5):
-            nums[x] = board[y*5+x]['score']
+            nums[y] = board[y*5+x]['score']
         scores[sc] = score_run(nums)
         sc += 1
 
     # diagonal TL -> BR
     for i in range(5):
-        nums[x] = board[5*i+i]['score']
+        nums[i] = board[5*i+i]['score']
     scores[sc] = score_run(nums)
     sc += 1
 
     # diagonal TR -> BL
     for i in range(5):
-        nums[x] = board[5*i + (5 - i)]['score']
+        nums[i] = board[5*i + (5 - i)]['score']
     scores[sc] = score_run(nums)
     sc += 1
 
